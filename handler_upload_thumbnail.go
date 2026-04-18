0b93c715bae51ae01a84a22c5c079a2f3ce086d6
VokalTuna <knutolavs@pm.me>
2026-04-18 19:42:25 +0200

    Have now made it so thumbnails are encoded in base64 and uploaded to our database in accordance to the course

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,37 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	fileData, fileHeaders, err := r.FormFile("thumbnail")
+	mediaType := fileHeaders.Header.Get("Content-Type")
+	imageData, err := io.ReadAll(fileData)
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find video", err)
+		return
+	}
+	if dbVideo.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not the owner of the video", nil)
+		return
+	}
+
+	encodedImageData := base64.StdEncoding.EncodeToString(imageData)
+	thumbnailURL := fmt.Sprintf("data:%s;base64,%s", mediaType, encodedImageData)
+	dbVideo.ThumbnailURL = &thumbnailURL
+	err = cfg.db.UpdateVideo(dbVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, dbVideo)
 }
