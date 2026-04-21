c635499e95fdfc56b192a02caaad529577265ddd
VokalTuna <knutolavs@pm.me>
2026-04-21 11:18:20 +0200

    Made changes align with solution of instructor version. Where mine implemented all logic in the thumbnail handler originally for everything. But solution made helper functions in the assets.go since getting the path to assets on disk, and creating names for files are pertaining to assets and is something external to the handler

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -39,24 +35,33 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// TODO: implement the upload here
 	const maxMemory = 10 << 20
-	err = r.ParseMultipartForm(maxMemory)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't parse", err)
-		return
-	}
+	r.ParseMultipartForm(maxMemory)
 
-	fileData, fileHeaders, err := r.FormFile("thumbnail")
+	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Not a proper request", err)
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
 	}
-	defer fileData.Close()
-	mediaType, _, err := mime.ParseMediaType(fileHeaders.Header.Get("Content-Type"))
+	defer file.Close()
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid content type.", err)
 		return
 	}
 	if mediaType != `image/png` && mediaType != `image/jpeg` {
-		respondWithError(w, http.StatusBadRequest, "Not a valid file format.", nil)
+		respondWithError(w, http.StatusBadRequest, "Invalid file type.", nil)
+		return
+	}
+	assetPath := getAssetPath(mediaType)
+	assetDiskPath := cfg.getAssetDiskPath(assetPath)
+
+	dst, err := os.Create(assetDiskPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file on server", err)
+		return
+	}
+	defer dst.Close()
+	if _, err = io.Copy(dst, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving file", err)
 		return
 	}
 
@@ -66,42 +71,16 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 	if dbVideo.UserID != userID {
-		respondWithError(w, http.StatusUnauthorized, "Not the owner of the video", nil)
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
 		return
 	}
 
-	parts := strings.Split(mediaType, "/")
-	if len(parts) != 2 {
-		respondWithError(w, http.StatusBadRequest, "No extension provided", nil)
-		return
-	}
-	extension := parts[1]
-	key := make([]byte, 32)
-	rand.Read(key)
-	videoName := base64.RawURLEncoding.EncodeToString(key)
-	thumbnailFileName := fmt.Sprintf("%s.%s", videoName, extension)
-	thumbnailFilePath := filepath.Join(cfg.assetsRoot, thumbnailFileName)
-	newFile, err := os.Create(thumbnailFilePath)
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could not write to storage", err)
-		return
-	}
-	defer newFile.Close()
-	_, err = io.Copy(newFile, fileData)
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could not write to storage", err)
-		return
-	}
-
-	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, thumbnailFileName)
-	dbVideo.ThumbnailURL = &thumbnailURL
+	url := cfg.getAssetURL(assetPath)
+	dbVideo.ThumbnailURL = &url
 	err = cfg.db.UpdateVideo(dbVideo)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
 		return
 	}
-
 	respondWithJSON(w, http.StatusOK, dbVideo)
 }
